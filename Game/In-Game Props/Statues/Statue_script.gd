75720ae88d2fc578ecc68ffecc8c09eb18906c50
IvanGiovany <113437934+IvanGiovany@users.noreply.github.com>
2024-06-19 19:39:02 +1000

    Added Statue to Introduction

---
+++ Game/In-Game Props/Statues/Statue_script.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+class_name Angel
+func _ready():
+	print("here")
+
+
+
+func _on_area_2d_area_entered(area):
+	self.z_index = 0
+
+
+func _on_area_2d_area_exited(area):
+	self.z_index = -1 # Replace with function body.
